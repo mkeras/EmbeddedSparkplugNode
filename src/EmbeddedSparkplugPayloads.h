47c0207ccb487840a83a36032ab12421b66d5386
mkeras <63744162+mkeras@users.noreply.github.com>
2024-01-17 15:34:32 -0800

    Initial commit

---
+++ src/EmbeddedSparkplugPayloads.h
@@ -0,0 +1,34 @@
+/*
+Copyright 2024 Michael Keras
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#ifndef EMBEDDED_SPARKPLUG_PAYLOADS_H
+#define EMBEDDED_SPARKPLUG_PAYLOADS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "pb_encode.h"
+#include "pb_decode.h"
+#include "sparkplug.pb.h"
+#include <BasicTag.h>
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif // EMBEDDED_SPARKPLUG_PAYLOADS_H
